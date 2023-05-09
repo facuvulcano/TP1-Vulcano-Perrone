f644cc6946da32edb89d5997e6e6e91e53d3113b
facuvulcano <110638331+facuvulcano@users.noreply.github.com>
2023-05-09 16:44:11 -0300

    Update MarsRoverTest.java

--- tp3 copy/MarsRoverTest.java
+++ tp3 copy/MarsRoverTest.java
@@ -10,40 +10,93 @@ import org.junit.Test;
 public class MarsRoverTest {
 
 	
-	@Test public void test00() {
+	
+	  @Test public void test00() {
 	  
 	  
-		Point initialPosition = new Point(1,1, "N"); 
-		MarsRover rover = new MarsRover(initialPosition);
+	  Point initialPosition = new Point(1,1, "N"); MarsRover rover = new
+	  MarsRover(initialPosition);
 	  
-		assertEquals(initialPosition, rover.getPosition());
-		assertTrue(rover.isHeadingNorth()); }
-	 
+	  assertEquals(initialPosition.point_array(), rover.getPosition());
+	  assertTrue(rover.isHeadingNorth()); }
+	  
 	 
 		
-	@Test public void test01() {
-		  
-		Point initialPosition = new Point(1,1, "N");
-		MarsRover rover = new MarsRover(initialPosition);
-		  
-		//rover.move("");
-		  
-		assertEquals(initialPosition, rover.getPosition());
-		assertTrue(rover.isHeadingNorth()); }
-		 
-	 
-	 @Test public void test02() {
-		  
-		Point initialPosition = new Point(1, 1, "N");
-		Point onePositionForward = new Point(2, 1, "N");
-		  
-		MarsRover rover1 = new MarsRover(initialPosition);
-		MarsRover rover2 = new MarsRover(onePositionForward);
-		rover1.move("f");
-		List<Object>positionRover1 = rover1.getPosition(); 
-		List<Object>positionRover2 = rover2.getPosition();
-		
-		assertEquals(positionRover1, positionRover2);
-		  
+	
+	/*
+	 * @Test public void test01() {
+	 * 
+	 * Point initialPosition = new Point(1,1, "N"); MarsRover rover = new
+	 * MarsRover(initialPosition);
+	 * 
+	 * rover.move("");
+	 * 
+	 * assertEquals(initialPosition.point_array(), rover.getPosition());
+	 * assertTrue(rover.isHeadingNorth()); }
+	 */
+	  
+	
+	
+	  @Test public void test02() {
+	  
+		  Point initialPosition = new Point(1, 1, "N"); 
+		  Point onePositionForward = new Point(2, 1, "N");
+	  
+		  MarsRover rover1 = new MarsRover(initialPosition); 
+		  MarsRover rover2 = new MarsRover(onePositionForward);
+	  
+	  
+		  List<Integer>positionRover2 = rover2.getPosition();
+	  
+		  assertEquals(positionRover2, rover1.move("f"));
+	  
 	  }
+	 
+	  @Test public void test03() {
+		  
+		  Point initialPosition = new Point(1, 1, "S"); 
+		  Point onePositionForward = new Point(0, 1, "S");
+	  
+		  MarsRover rover1 = new MarsRover(initialPosition); 
+		  MarsRover rover2 = new MarsRover(onePositionForward);
+	  
+	  
+		  List<Integer>positionRover2 = rover2.getPosition();
+	  
+		  assertEquals(positionRover2, rover1.move("f"));
+	  
+	  }
+	  
+	  @Test public void test04() {
+		  
+		  Point initialPosition = new Point(1, 1, "W"); 
+		  Point onePositionForward = new Point(1, 0, "W");
+	  
+		  MarsRover rover1 = new MarsRover(initialPosition); 
+		  MarsRover rover2 = new MarsRover(onePositionForward);
+	  
+	  
+		  List<Integer>positionRover2 = rover2.getPosition();
+	  
+		  assertEquals(positionRover2, rover1.move("f"));
+	  
+	  }
+	 
+	  @Test public void test05() {
+		  
+		  Point initialPosition = new Point(1, 1, "E"); 
+		  Point onePositionForward = new Point(1, 2, "E");
+	  
+		  MarsRover rover1 = new MarsRover(initialPosition); 
+		  MarsRover rover2 = new MarsRover(onePositionForward);
+	  
+	  
+		  List<Integer>positionRover2 = rover2.getPosition();
+	  
+		  assertEquals(positionRover2, rover1.move("f"));
+	  
+	  }
+	 
+	 
+	 
 }
